e9f6207c86e57c24ca6f6f2196631193a8591fc1
washington.riega <washington.riega@itriad.org.br>
2023-07-18 18:40:57 -0400

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
-module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}',
-    ],
-    coverageDirectory: 'coverage',
-    testEnviroment: 'node',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    }
+
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
+module.exports = config
